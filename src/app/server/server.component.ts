cbfb16c6bceade0f6d97b0c34809c4a694f7a2a1
pekuria <pekuria@hotmail.com>
2020-10-28 02:57:25 +0000

    fix(server.component.ts): chenged double quotes to single quotes

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,8 @@
-import {Component} from "@angular/core";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: "./server.component.html",
+  templateUrl: './server.component.html',
   styles: [
     `.online {
         color: white;
